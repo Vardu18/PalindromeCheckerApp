b57d4021e579804831542321439ee424f62290be
Vardu18 <varadsg236@gmail.com>
2026-02-24 22:28:40 +0530

    UC2 comits

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -3,5 +3,30 @@ public class PalindromeCheckerApp {
         System.out.println ("Welcome to the Palindrome Checker management system");
         System.out.println ("Version : 1.0");
         System.out.println ("System initialized Successfully");
-    }
+
+        //USecase 2
+        public class UseCase2PalindromeCheckerApp {
+
+            public static void main(String[] args) {
+
+                // Hardcoded string
+                String word = "madam";
+
+                // Reverse the string
+                String reversed = "";
+                for (int i = word.length() - 1; i >= 0; i--) {
+                    reversed = reversed + word.charAt(i);
+                }
+
+                // Check if palindrome
+                if (word.equals(reversed)) {
+                    System.out.println(word + " is a Palindrome.");
+                } else {
+                    System.out.println(word + " is not a Palindrome.");
+                }
+            }
+        }
+
+
+
 }
